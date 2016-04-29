a12d38ba2cfb675759b655aea61a70ac4888462f
Brian Stickney <brianchase055@gmail.com>
2016-04-29 14:46:46 -0500

    Migrations and Minor changes

---
+++ SurfPaddleHTML/Migrations/201604291907485_inital migration.cs
@@ -0,0 +1,29 @@
+namespace SurfPaddleHTML.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initalmigration : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.BlogPosts",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Author = c.String(),
+                        Title = c.String(),
+                        Body = c.String(),
+                        DateCreated = c.DateTime(nullable: false),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.BlogPosts");
+        }
+    }
+}
